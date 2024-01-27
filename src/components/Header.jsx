fe0353870f5fee07c084a06dda1760f9b9d3af7a
Anthony Hrabowski <ajhrabowski@gmail.com>
2024-01-27 13:03:51 -0500

    Made the header responsive and added media query to make it compatable on multiple screens.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 import Navigation from './Navigation'
 
 function Header() {
+  const showMenu = () => {
+    const nav = document.querySelector("nav")
+    if (nav.style.display == "none") {
+      nav.style.display = "flex"
+    } else {
+      nav.style.display = "none"
+    }
+  }
+  window.onresize = () => {
+    if (screen.width > 780) {
+      const nav = document.querySelector("nav")
+      nav.style.display = "flex"
+    }
+  }
   return (
     <header>
-        <h1>Anthony Hrabowski</h1>
-        <Navigation/>
+      <h1>Anthony Hrabowski</h1>
+      <i
+        onClick={showMenu}
+        className="fa-solid fa-bars" id='menu-bar'></i>
+      <Navigation />
     </header>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,18 @@
 // Bringing in the required import from 'react-router-dom'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 export default function Navigation() {
-  // The Navbar UI component will render each of the Link elements in the links prop
+  // The Navbar UI component will render each of the NavLink elements in the NavLinks prop
   return (
     <nav>
-      <Link to="/">About Me</Link>
-      <Link to="/portfolio">Portfolio</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/resume">Resume</Link>
+      <NavLink className={({ isActive }) => (isActive ? "active-link" : "")} to="/">About Me</NavLink>
+      <NavLink className={({ isActive }) => (isActive ? "active-link" : "")} to="/portfolio">Portfolio</NavLink>
+      <NavLink className={({ isActive }) => (isActive ? "active-link" : "")} to="/contact">Contact</NavLink>
+      <NavLink className={({ isActive }) => (isActive ? "active-link" : "")} to="/resume">Resume</NavLink>
     </nav>
   );
 }
+
+
 
