d8930614df3be7accb0ccc9b9ba2081bb0bb296b
Anthony Hrabowski <ajhrabowski@gmail.com>
2024-01-28 22:12:59 -0500

    Edited the Portfolio and About Page. Made the container for my projects along with links to Live and Github.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-function Project() {
+function Project({project}) {
   return (
-    <div>Project</div>
+    <div className='project'>
+      <img src={project.img} alt="showcase screenshot" />
+      <div>
+        <h4>{project.name}</h4>
+        <a href={project.live} target="_blank" rel="noopener noreferrer">Live</a>
+        <a href={project.github} target="_blank" rel="noopener noreferrer">Github</a>
+      </div>
+    </div>
   )
 }
 
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import Project from '../components/Project'
+const projects = [
+  {
+    name:"Showcase",
+    img:"/showcase.png",
+    live:"https://benrodriguezmoran.github.io/country-curency-converter/",
+    github:"https://github.com/benrodriguezmoran/country-curency-converter"
+  },
+  {
+    name:"Showcase",
+    img:"/showcase.png",
+    live:"https://benrodriguezmoran.github.io/country-curency-converter/",
+    github:"https://github.com/benrodriguezmoran/country-curency-converter"
+  }
+]
 function PortfolioPage() {
   return (
-    <div>PortfolioPage</div>
+    <div className='portfolio-page'>
+      <h1>Portfolio</h1>
+      <div className="projects-container">
+       {
+        projects.map((proj,i)=><Project key={i} project={proj} />)
+       }
+      </div>
+    </div>
   )
 }
 
