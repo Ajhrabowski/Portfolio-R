138c557a015bdff07749382bdc425d5278eadc32
Anthony Hrabowski <ajhrabowski@gmail.com>
2024-01-31 12:59:54 -0500

    Updated my About Page.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,16 +6,16 @@ export default function About() {
         <img src="/Coach-Tony.jpg" alt="my profile picture" />
       </div>
       <p>
-        My name is Anthony Hrabbowski. I am currently a student attending a MSU coding bootcamp. I have had many talents in my career. Once refered to myself as "One Man Many Talents". I am a Barber, Dj and a Personal Trainer. I once was a Recording Engineer and dabbled in the field of Graphic design for a minute. The Grapic Design is what landed me here.
-        I became facinated with the Worldwide Web and how it looks. So I said to myself I want to design a website.
+        My name is Anthony Hrabbowski. I am currently a student attending a MSU coding bootcamp. I have had many talents in my career. Once referred to myself as "One Man Many Talents." I am a Barber, Dj and a Personal Trainer. I once was a Recording Engineer and dabbled in the field of Graphic design for a minute. The Grapic Design is what landed me here.
+        I became facinated with the Worldwide Web and how it looks. So I said to myself, I want to design a website.
         </p>
         
         <p>
-          I started with Wix and didn't quite like it that much. Decided to try the UX Design route. I liked it a little, However wanted to get more in depth with design and building of the site more so than all the case studying and interviewing. So I decided to attended Henry Ford Community College
+          At that time I strived to start with Wix and didn't quite like it that much. Afterwards I decided to try the UX Design route instead. I liked it a little, however I wanted to get more in depth with designing and building of the site, more so than all the case studying and interviewing. So I decided to attend Henry Ford Community College
           to start my journey as a Web Developer.
         </p>
         <p>
-        I completed one semester at Henry Ford and became so enthused and egar to get things moving that I decided to attended MSU's Full Stack Web Development Boot Camp through edX's Online Learning Platform. So here I am Anthony John Hrabowski, Student of the Art of Web Development attending MSU Coding Bootcamp.
+        I completed one semester at Henry Community Ford and became so enthused and eager to get things moving, so I decided to attend MSU's Full Stack Web Development Boot Camp, through edX's Online Learning Platform. So here I am Anthony John Hrabowski, Student of the Art of Web Development attending MSU Coding Bootcamp.
       </p>
     </div>
   );
